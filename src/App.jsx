1acb55b5b03b5c4431773404e47a896228499ee5
Siddharth <singhsiddharth6582@gmail.com>
2022-01-06 20:50:38 +0530

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,10 @@ import Login from "./Component/Login/Login";
 import HomePage from "./Component/HomePage Component/HomePage";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SignUp from "./Component/SignUp/SignUp";
-<<<<<<< HEAD
-import SignUp from "./Component/SignUp/SignUp";
 //Bc aaj to lode lag gye
 //lode toh lag gaye par rohit bechare ke
 //Test ho rha he
 function App() {
-=======
-function App() {
-  //test line by Sid
-  //I am abhi bro
->>>>>>> Sid
   return (
     <BrowserRouter>
       <Routes>
