33ada76b19b2f4ad213f7ce543d072204d59dd84
Abhi <abhiabhii2000@gmail.com>
2022-01-06 20:18:29 +0530

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import "./App.css";
 import Login from "./Component/Login/Login";
 import HomePage from "./Component/HomePage Component/HomePage";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
-//hello bro
-//hi bro - Sid
-//this is test branch
-=======
 import SignUp from "./Component/SignUp/SignUp";
->>>>>>> 82859275910bbb647966f4700e2eb13d4c92b883
 function App() {
   //test line by Sid
   return (
@@ -17,7 +11,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-
       </Routes>
     </BrowserRouter>
   );
